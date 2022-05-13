0e95e04d00162a76da0aa7d4014f84c875df28a0
AntoniPizarro <apizarro@cifpfbmoll.eu>
2022-05-13 09:33:22 +0200

    mods+forge+instalador

---
+++ instalacion.bat
@@ -0,0 +1,36 @@
+@echo off
+:pre_ini
+git pull
+
+:inicio
+cls
+echo.
+echo [1] Instalar SOLO mods
+echo [2] Instalar SOLO forge
+echo [3] Instalar forge y mods
+echo.
+set /p accion=...
+
+if %accion% == 1 goto mods
+if %accion% == 2 goto forge
+if %accion% == 3 goto ambos
+
+:forge
+cls
+java -jar forge-1.18.2-40.1.0-installer.jar nogui
+goto salir
+
+:mods
+cls
+xcopy mods C:\Users\%username%\AppData\Roaming\.minecraft\mods /D
+goto salir
+
+:ambos
+cls
+java -jar forge-1.18.2-40.1.0-installer.jar nogui
+xcopy mods C:\Users\%username%\AppData\Roaming\.minecraft\mods /D
+goto salir
+
+:salir
+cls
+exit
